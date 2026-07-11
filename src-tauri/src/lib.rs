4f0850ba5569eca3d78c9a8cbf46c7e8e5a57640
minhdc <minhdc@vacom.com.vn>
2026-07-11 14:25:23 +0700

    ....

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,6 +18,7 @@ use tauri::{
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::init(
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
             None,
