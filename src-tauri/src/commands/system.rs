8930291246a85601356261f3950c361d53ea1c5c
minhdc <minhdc@vacom.com.vn>
2026-07-16 17:17:54 +0700

    ...

--- src-tauri/src/commands/system.rs
+++ src-tauri/src/commands/system.rs
@@ -48,10 +48,8 @@ pub fn page_ready(name: String, show: Option<bool>) {
     if let Some(app) = APP_HANDLE.get() {
         if let Some(window) = app.get_webview_window("main") {
             let show = show.unwrap_or(true);
-            let (width, height) = window_config::get_window_size(&name).unwrap();
+            let (mut width, mut height) = window_config::get_window_size(&name).unwrap();
             if show {
-                let _ = window.set_size(Size::Logical(LogicalSize { width, height }));
-                // let _ = window.center();
                 if let Some(monitor) = window.current_monitor().unwrap() {
                     let scale = monitor.scale_factor();
                     let monitor_size = monitor.size();
@@ -59,14 +57,14 @@ pub fn page_ready(name: String, show: Option<bool>) {
                     let monitor_width = monitor_size.width as f64 / scale;
                     let monitor_height = monitor_size.height as f64 / scale;
 
-                    let max_width = monitor_width * 0.90;
-                    let max_height = monitor_height * 0.90;
+                    width = width.min(monitor_width * 0.9);
+                    height = height.min(monitor_height * 0.9);
 
-                    let width = width.min(max_width);
-                    let height = height.min(max_height);
+                    let _ = window.set_size(Size::Logical(LogicalSize { width, height }));
 
                     let x = (monitor_width - width) / 2.0;
                     let y = (monitor_height - height) / 2.0;
+
                     let _ = window.set_position(Position::Logical(LogicalPosition { x, y }));
                 }
                 let can_resize = matches!(name.as_str(), "report" | "***");
